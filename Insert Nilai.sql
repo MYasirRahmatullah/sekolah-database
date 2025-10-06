4bc114f906e598d0f5f3a877d3800430e34f9c19
Yasir <email_anda@example.com>
2025-10-06 14:54:42 +0700

    Update insert siswa dan insert nilai

--- Insert Nilai.sql
+++ Insert Nilai.sql
@@ -1,2 +1,2 @@
 INSERT INTO nilai (siswa_id, mata_pelajaran, nilai) 
-VALUES (1, 'Matematika', 85), (2, 'Bahasa Inggris', 90);
+VALUES (1, 'Matematika', 85),(2, 'Bahasa Inggris', 90),(3, 'Biologi', 90), (4, 'Fisika', 85),(5, 'Kimia', 80); 
--- Insert Siswa.sql
+++ Insert Siswa.sql
@@ -1,2 +1,2 @@
 INSERT INTO siswa (nama, umur, jurusan)
-VALUES ('Andi', '16', 'IPA'),('Budi', '17', 'IPS');
+VALUES ('Andi', '16', 'IPA'),('Budi', '17', 'IPS'),('Toni', '17', 'BAHASA'),('Surya', '16', 'IPA'),('Deni', '16', 'IPS');
